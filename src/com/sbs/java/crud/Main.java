7f0170b9dbaa8b7b9fbd2d667d649ce15629a893
su2333066 <gisu980130@gmail.com>
2021-07-19 12:50:42 +0900

    추가 리스팅

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -50,7 +50,33 @@ public class Main {
 					Article article = articles.get(i);
 					System.out.printf("%d   |   %s\n", article.id, article.title);
 				}
-			} else {
+			} else if(command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+				
+				int id = Integer.parseInt(commandBits[2]);
+				boolean foundArticle = false;
+				
+				for(int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					
+					if(article.id == id) {
+						foundArticle = true;
+						break;
+					}
+				}
+				
+				if(foundArticle == false) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				else {
+					System.out.printf("%d번 게시물은 존재합니다.\n", id);
+					continue;
+				}
+				
+				
+			}
+			else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
 		}
